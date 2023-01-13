f1190d3c6f9841b5bfe1e77cc47a883aea9121e7
Kdsingh333 <90763155+Kdsingh333@users.noreply.github.com>
2023-01-13 23:36:33 +0530

    13-01-23

---
+++ 1-Jan/230-KthSmallestElementinaBST.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> q = new ArrayList<>();
+         inorder(root,q);
+
+         return q.get(k-1);
+      
+       
+    }
+
+    public List<Integer> inorder(TreeNode root, List<Integer> q){
+        if(root == null){
+            return null;
+        }
+        inorder(root.left,q);
+        q.add(root.val);
+        inorder(root.right,q);
+
+        return q;
+    }
+}
